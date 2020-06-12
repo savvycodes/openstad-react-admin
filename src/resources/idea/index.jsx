1b3fc6d3d41651c32422227686085ff870cb89ff
mitkoevoets <mitkoevoets@vaerens.com>
2020-06-12 15:56:03 +0200

    Refactor

--- src/resources/idea/index.jsx
+++ src/resources/idea/index.jsx
@@ -1,32 +1,6 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import BookIcon from '@material-ui/icons/Book';
 
 export const IdeaIcon = BookIcon;
 export { IdeaList } from './list';
 export { IdeaEdit, IdeaCreate } from './create-edit';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    paddingTop: 40,
-  },
-  title: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    margin: '1em',
-  },
-  form: {
-    [theme.breakpoints.up('xs')]: {
-      width: 400,
-    },
-    [theme.breakpoints.down('xs')]: {
-      width: '100vw',
-      marginTop: -30,
-    },
-  },
-  inlineField: {
-    display: 'inline-block',
-    width: '50%',
-  },
-}));
---
+++ src/resources/idea/useStyles.jsx
@@ -0,0 +1,26 @@
+// TODO: @DEV unused - Can be removed?
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    paddingTop: 40,
+  },
+  title: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    margin: '1em',
+  },
+  form: {
+    [theme.breakpoints.up('xs')]: {
+      width: 400,
+    },
+    [theme.breakpoints.down('xs')]: {
+      width: '100vw',
+      marginTop: -30,
+    },
+  },
+  inlineField: {
+    display: 'inline-block',
+    width: '50%',
+  },
+}));

1b3e4ba61387ae362275194bd5c32f0bbf56c8b6
mitkoevoets <mitkoevoets@vaerens.com>
2020-06-13 12:36:52 +0200

    Refine import

--- src/components/ImportButton/validateCsv.js
+++ src/components/ImportButton/validateCsv.js
@@ -1,40 +1,47 @@
 export default async (csvRows, schema) => {
-  let validationErrors = [];
+  let validationMessages = [];
 
   if (!csvRows.length > 0) {
     return {
-      errorType: 'schemaError',
+      messageType: 'zeroRows',
+      color: 'black',
       message: `There are no rows in the file`,
     };
   }
 
+  /**
+   * Check for id
+   */
+  if(csvRows[0].hasOwnProperty('id')) {
+    validationMessages.push({
+      messageType: 'idColumn',
+      color: 'black',
+      message: 'Id column is present in file; saving will only work with overwrite',
+    });
+  }
+
+  /**
+   * Check for id
+   */
+  if(csvRows[0].hasOwnProperty('id')) {
+    validationMessages.push({
+      messageType: 'idColumn',
+      color: 'black',
+      message: 'Id column is present in file; saving will only work with overwrite',
+    });
+  }
+
+  /**
+   * Validate schema
+   */
   Object.keys(schema).forEach((key) => {
     if (!csvRows[0].hasOwnProperty(key))
-      validationErrors.push({
-        errorType: 'schemaError',
+      validationMessages.push({
+        messageType: 'schemaError',
+        color: 'red',
         message: `Import is missing the following column: ${key}`,
       });
   });
 
-  return validationErrors;
-
-  // const values = Object.keys(csvRows).map((row) => {
-  //   let cvsValidationErrors = [];
-  //
-  //   Object.keys(schema).forEach((key) => {
-  //     if (row.hasOwnProperty(key))
-  //       cvsValidationErrors.push({
-  //         key,
-  //         errorType: 'schemaError',
-  //         message: 'There was a validation error',
-  //       });
-  //   });
-  //
-  //   return {
-  //     cvsValidationErrors,
-  //     ...row,
-  //   }
-  // })
-  //
-  // setValues(values);
+  return validationMessages;
 };

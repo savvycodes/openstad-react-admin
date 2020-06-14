cb15771692ac9bb8436445783030293b36f1ce0d
mitkoevoets <mitkoevoets@vaerens.com>
2020-06-14 14:34:03 +0200

    Refactor

--- src/components/ImportButton/ImportRowCountLine.jsx
+++ src/components/ImportButton/ImportRowCountLine.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Zoom from '@material-ui/core/Zoom';
+import LightTooltip from '../LightTooltip';
 
 export default (props) => {
   const { values } = props;
 
   return (<>
     {!!values && (
-      <p style={{ marginBottom: '0px' }}>
-        {'Import row count'}: <strong>{values.length}</strong>
-      </p>
+      <LightTooltip title={
+        <React.Fragment>
+          <Typography color="inherit">
+            {
+              values.map((value) => {
+                return (<p>{`${value}`}</p>);
+              })
+            }
+          </Typography>
+        </React.Fragment>
+      } TransitionComponent={Zoom} interactive arrow placement="top">
+        <p style={{ marginBottom: '0px' }}>
+          {'Import row count'}: <strong>{values.length}</strong>
+        </p>
+      </LightTooltip>
     )}
   </>);
 }

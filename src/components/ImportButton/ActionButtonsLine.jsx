541e1238aa7b58cf6300bca1ee673a8f1842c2d0
mitkoevoets <mitkoevoets@vaerens.com>
2020-06-18 15:28:36 +0200

    Content adjustment

--- src/components/ImportButton/ActionButtonsLine.jsx
+++ src/components/ImportButton/ActionButtonsLine.jsx
@@ -10,7 +10,7 @@ export default (props) => {
   return (
     <>
       <Button onClick={handleClose}>
-        <span>{'cancel'}</span>
+        <span>{'close'}</span>
       </Button>
       <Button
         disabled={!values || values.length < 1 || importing || idPresent}

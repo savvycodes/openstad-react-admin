3247a2f2dd822908b6d7bbe7c251ecae09a506f9
mitkoevoets <mitkoevoets@vaerens.com>
2020-06-18 19:51:12 +0200

    Refactor create-edit.jsx

--- src/resources/idea/create-edit.jsx
+++ src/resources/idea/create-edit.jsx
@@ -18,73 +18,71 @@ const IdeaTitle = ({ record }) => {
   return <span>Idea {record ? `"${record.title}"` : ''}</span>;
 };
 
+const Form = (props) => (
+  <TabbedForm>
+    <FormTab label="Info">
+      <TextInput disabled source="id"/>
+      <ReferenceInput label="User" source="userId" reference="user" variant="outlined">
+        <SelectInput optionText="email"/>
+      </ReferenceInput>
+    {/*  <TextInput source="title" variant="outlined" fullWidth/>*/}
+    {/*  <TextInput source="summary" options={{ multiLine: true }} variant="outlined" fullWidth/>*/}
+    {/*  <TextInput multiline source="description" variant="outlined" fullWidth/>*/}
+    {/*  <ReferenceArrayInput label="tags" source="tags" reference="tag" variant="outlined">*/}
+    {/*    <SelectArrayInput optionText="name"/>*/}
+    {/*  </ReferenceArrayInput>*/}
+
+    {/*  <h3>Image (TODO)</h3>*/}
+    {/*  <FileUpload resourceProps={props} imageApiUrl={props.options.imageApiUrl}/>*/}
+    {/*</FormTab>*/}
+    {/*<FormTab label="Extradata">*/}
+    {/*  <TextInput disabled source="id"/>*/}
+    {/*  <JsonInput source="extraData"/>*/}
+    {/*</FormTab>*/}
+    {/*<FormTab label="Comments">*/}
+    {/*  <ReferenceManyField*/}
+    {/*    reference="argument"*/}
+    {/*    target="ideaId"*/}
+    {/*    addLabel={false}*/}
+    {/*    pagination={<Pagination/>}*/}
+    {/*    fullWidth*/}
+    {/*  >*/}
+    {/*    <Datagrid>*/}
+    {/*      <TextField source="id"/>*/}
+    {/*      <TextField source="description"/>*/}
+    {/*      <FunctionField label="Author" render={record => `${record.user.firstName} ${record.user.lastName}`}/>*/}
+    {/*      <DateField source="createdAt"/>*/}
+    {/*      <EditButton basePath="/Argument"/>*/}
+    {/*    </Datagrid>*/}
+    {/*  </ReferenceManyField>*/}
+    {/*</FormTab>*/}
+    {/*<FormTab label="Votes">*/}
+    {/*  <ReferenceManyField*/}
+    {/*    reference="vote"*/}
+    {/*    target="ideaId"*/}
+    {/*    addLabel={false}*/}
+    {/*    pagination={<Pagination/>}*/}
+    {/*    fullWidth*/}
+    {/*  >*/}
+    {/*    <Datagrid>*/}
+    {/*      <TextField source="id"/>*/}
+    {/*      <TextField source="userId"/>*/}
+    {/*      <DateField source="createdAt"/>*/}
+    {/*      <EditButton basePath="/Vote"/>*/}
+    {/*    </Datagrid>*/}
+    {/*  </ReferenceManyField>*/}
+    </FormTab>
+  </TabbedForm>
+);
+
 export const IdeaEdit = (props) => (
   <Edit title={<IdeaTitle/>} {...props}>
-    <TabbedForm>
-      <FormTab label="Info">
-        <TextInput disabled source="id"/>
-        <ReferenceInput label="User" source="userId" reference="user" variant="outlined">
-          <SelectInput optionText="email"/>
-        </ReferenceInput>
-        <TextInput source="title" variant="outlined" fullWidth/>
-        <TextInput source="summary" options={{ multiLine: true }} variant="outlined" fullWidth/>
-        <TextInput multiline source="description" variant="outlined" fullWidth/>
-        <ReferenceArrayInput label="tags" source="tags" reference="tag" variant="outlined">
-          <SelectArrayInput optionText="name"/>
-        </ReferenceArrayInput>
-
-        <h3>Image (TODO)</h3>
-        <FileUpload resourceProps={props} imageApiUrl={props.options.imageApiUrl}/>
-      </FormTab>
-      <FormTab label="Extradata">
-        <TextInput disabled source="id"/>
-        <JsonInput source="extraData"/>
-      </FormTab>
-      <FormTab label="Comments">
-        <ReferenceManyField
-          reference="argument"
-          target="ideaId"
-          addLabel={false}
-          pagination={<Pagination/>}
-          fullWidth
-        >
-          <Datagrid>
-            <TextField source="id"/>
-            <TextField source="description"/>
-            <FunctionField label="Author" render={record => `${record.user.firstName} ${record.user.lastName}`}/>
-            <DateField source="createdAt"/>
-            <EditButton basePath="/Argument"/>
-          </Datagrid>
-        </ReferenceManyField>
-      </FormTab>
-      <FormTab label="Votes">
-        <ReferenceManyField
-          reference="vote"
-          target="ideaId"
-          addLabel={false}
-          pagination={<Pagination/>}
-          fullWidth
-        >
-          <Datagrid>
-            <TextField source="id"/>
-            <TextField source="userId"/>
-            <DateField source="createdAt"/>
-            <EditButton basePath="/Vote"/>
-          </Datagrid>
-        </ReferenceManyField>
-      </FormTab>
-    </TabbedForm>
+    <Form {...props}/>
   </Edit>
 );
 
 export const IdeaCreate = (props) => (
   <Create title="Create a Idea" {...props}>
-    <SimpleForm>
-      <TextInput source="title"/>
-      <TextInput source="teaser" options={{ multiLine: true }}/>
-      <TextInput multiline source="description"/>
-      {/*<TextInput label="Publication date" source="published_at"/>*/}
-      {/*<TextInput source="average_note"/>*/}
-    </SimpleForm>
+    <Form {...props}/>
   </Create>
 );

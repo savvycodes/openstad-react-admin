9da88178e62763d48315cdbe590a8dbcc54e3226
mitkoevoets <mitkoevoets@vaerens.com>
2020-06-21 16:19:34 +0200

    Add BackButton

---
+++ src/components/BackButton/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Button from '@material-ui/core/Button';
+import { goBack } from 'react-router-redux';
+
+class BackButton extends Component {
+  handleClick = () => {
+    this.props.goBack();
+  };
+
+  render() {
+    return <Button variant="contained" color="primary" onClick={this.handleClick}>Go Back</Button>;
+  }
+}
+
+export default connect(null, {
+  goBack,
+})(BackButton);

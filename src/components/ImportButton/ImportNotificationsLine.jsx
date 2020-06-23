b94c846d9cb1d0314f8b1c57975ed668b1c3f1b5
mitkoevoets <mitkoevoets@vaerens.com>
2020-06-23 10:37:21 +0200

    Add info button to modal

--- src/components/ImportButton/ImportNotificationsLine.jsx
+++ src/components/ImportButton/ImportNotificationsLine.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Zoom from '@material-ui/core/Zoom';
 import LightTooltip from '../LightTooltip';
+import InfoOutlined from '@material-ui/icons/InfoOutlined';
 
 export default (props) => {
   const { csvValidationNotifications, dialogStatus } = props;
@@ -28,28 +29,41 @@ export default (props) => {
 
   return (<>
     {!!csvValidationNotifications && csvValidationNotifications.length > 0 && (
-      <LightTooltip title={
-        <React.Fragment>
-          <Typography color="inherit">
-            {
-              csvValidationNotifications.map((validationError, index) => {
-                return (
-                  <span
-                    key={index}
-                    style={{ color: validationError.color, display: 'block' }}
-                  >
-                    {`${validationError.message}`}
-                  </span>
-                );
-              })
-            }
-          </Typography>
-        </React.Fragment>
-      } TransitionComponent={Zoom} interactive arrow placement="top">
-        <p style={{ marginBottom: '0px', color: 'blue' }}>
+      <>
+        <p style={{ marginBottom: '0px', color: 'blue', display: 'inline-block' }}>
           {'Import validation alerts'}: <strong>{csvValidationNotifications.length}</strong>
         </p>
-      </LightTooltip>
+        <LightTooltip title={
+          <React.Fragment>
+            <Typography color="inherit">
+              {
+                csvValidationNotifications.map((validationError, index) => {
+                  return (
+                    <span
+                      key={index}
+                      style={{ color: validationError.color, display: 'block' }}
+                    >
+                      {`${validationError.message}`}
+                    </span>
+                  );
+                })
+              }
+            </Typography>
+          </React.Fragment>
+        } TransitionComponent={Zoom} interactive arrow placement="top">
+          <span
+            style={{
+              verticalAlign: 'middle',
+              padding: '8px',
+              cursor: 'pointer',
+              color: 'blue',
+            }}
+            onClick={() => console.log('sss')}
+          >
+          <InfoOutlined/>
+          </span>
+        </LightTooltip>
+      </>
     )}
   </>);
 }

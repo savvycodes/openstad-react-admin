3853264d107d6039fe8f6f0f7f62befb71e986f2
mitkoevoets <mitkoevoets@vaerens.com>
2020-06-23 10:43:07 +0200

    Add info button to row count line

--- src/components/ImportButton/ImportRowCountLine.jsx
+++ src/components/ImportButton/ImportRowCountLine.jsx
@@ -2,32 +2,46 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Zoom from '@material-ui/core/Zoom';
 import LightTooltip from '../LightTooltip';
+import InfoOutlined from '@material-ui/icons/InfoOutlined';
 
 export default (props) => {
   const { values } = props;
 
   return (<>
     {!!values && (
-      <LightTooltip title={
-        <React.Fragment>
-          <Typography color="inherit">
-            {
-              values.map((row, rIndex) => {
-                return (<span key={rIndex} style={{ color: 'black', display: 'block'}}>
+      <>
+        <p style={{ marginBottom: '0px', color: 'blue', display: 'inline-block' }}>
+          {'Import row count'}: <strong>{values.length}</strong>
+        </p>
+        <LightTooltip title={
+          <React.Fragment>
+            <Typography color="inherit">
+              {
+                values.map((row, rIndex) => {
+                  return (<span key={rIndex} style={{ color: 'black', display: 'block'}}>
                   {Object.keys(row).map((key, cIndex) => {
                     return (<span key={cIndex} >{`${row[key]}`}</span>);
                   })}
-                  <hr/>
+                    <hr/>
                 </span>)
-              })
-            }
-          </Typography>
-        </React.Fragment>
-      } TransitionComponent={Zoom} interactive arrow placement="top">
-        <p style={{ marginBottom: '0px' }}>
-          {'Import row count'}: <strong>{values.length}</strong>
-        </p>
-      </LightTooltip>
+                })
+              }
+            </Typography>
+          </React.Fragment>
+        } TransitionComponent={Zoom} interactive arrow placement="top">
+          <span
+            style={{
+              verticalAlign: 'middle',
+              padding: '8px',
+              cursor: 'pointer',
+              color: 'blue',
+            }}
+            onClick={() => console.log('sss')}
+          >
+          <InfoOutlined/>
+          </span>
+        </LightTooltip>
+      </>
     )}
   </>);
 }

e144d84cb8e3fcd888d3b0f295433aba09c125f2
mitkoevoets <mitkoevoets@vaerens.com>
2020-07-27 19:55:27 +0200

    Add Filters for arguments

--- src/resources/argument.jsx
+++ src/resources/argument.jsx
@@ -1,12 +1,20 @@
 // in posts.js
 import React from 'react';
-import { List, NumberField, NumberInput, Datagrid, Edit, Create, SimpleForm, DateField, TextField, EditButton, TextInput, DateInput, ImageInput, ImageField, FunctionField } from 'react-admin';
+import { List, Filter, NumberInput, Datagrid, Edit, Create, SimpleForm, DateField, TextField, EditButton, TextInput, DateInput, ImageInput, ImageField, FunctionField } from 'react-admin';
 import ListAltIcon from '@material-ui/icons/ListAlt';
 
 export const ArgumentIcon = ListAltIcon;
 
+const ArgumentFilters = (props) => (
+  <Filter {...props}>
+    {/*<TextInput label="Search" source="q" alwaysOn />*/}
+    <TextInput label="Id" source="id" defaultValue="" />
+    <TextInput label="Description" source="description" defaultValue="" />
+  </Filter>
+);
+
 export const ArgumentList = (props) => (
-    <List {...props}  title="Argumenten">
+    <List {...props} filters={<ArgumentFilters />} title="Argumenten">
         <Datagrid>
             <TextField source="id" />
             <TextField source="description" />

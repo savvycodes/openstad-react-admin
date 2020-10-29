8b2c8a9ff48cd9ae68b5fd5b18ca00923cd9e340
Niels Vegter <niels@denes.nl>
2020-10-29 13:32:58 +0000

    Update build options

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,22 @@ module.exports = function (api) {
   api.cache(true);
 
   const presets = [
-		"@babel/preset-env",
-		"@babel/preset-react",
+		["@babel/preset-env", {
+			targets: {
+				edge: "17",
+				ie: '11',
+				firefox: "60",
+				chrome: "67",
+				safari: "11.1",
+			},
+    }],
+		["@babel/preset-react", {
+    }],
 	];
 
   const plugins = [
+    "@babel/plugin-transform-parameters",
+    "@babel/plugin-transform-arrow-functions",
  		[
 			"react-css-modules",
 			{

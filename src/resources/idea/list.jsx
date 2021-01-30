5c708d05878f79c48a7e4fc4e06be31fbbcfe0d0
mitkoevoets <mitkoevoets@vaerens.com>
2021-01-30 09:46:52 +0100

    Working on dialog

--- src/resources/idea/list.jsx
+++ src/resources/idea/list.jsx
@@ -1,18 +1,25 @@
-import React, { Fragment, cloneElement } from 'react';
-import { Datagrid, Filter, DateField, EditButton, ImageField, TextInput, List, TextField, TopToolbar,
-  downloadCSV, useListContext, BulkDeleteButton, SimpleForm, SaveButton } from 'react-admin';
+import React, {Fragment, cloneElement, useState} from 'react';
+import {
+    Datagrid, Filter, DateField, EditButton, ImageField, TextInput, List, TextField, TopToolbar,
+    downloadCSV, useListContext, BulkDeleteButton, SimpleForm, SaveButton
+} from 'react-admin';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import { ImportButton } from '../../components/ImportButton/index.jsx';
+import {makeStyles} from '@material-ui/core/styles';
+import {ImportButton} from '../../components/ImportButton/index.jsx';
 import Inbox from '@material-ui/icons/Inbox';
-import { Button as RAButton } from 'react-admin';
+import {Button, Confirm} from 'react-admin';
 import ContentCreate from '@material-ui/icons/Create';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
-import {ReferenceInput, SelectInput} from 'react-admin';
+import {
+    ReferenceInput, SelectInput, useUpdateMany,
+    useRefresh,
+    useNotify,
+    useUnselectAll,
+} from 'react-admin';
 
-import { CreateButton, ExportButton } from 'ra-ui-materialui';
+import {CreateButton, ExportButton} from 'ra-ui-materialui';
 // in PostList.js
 import jsonExport from 'jsonexport/dist';
 import dataProvider from "../../dataProvider";
@@ -37,18 +44,18 @@ const useStyles = makeStyles(
             marginTop: '2em',
         },
     }),
-    { name: 'RaEmpty' }
+    {name: 'RaEmpty'}
 );
 
 const exporter = posts => {
     const postsForExport = posts.map(post => {
-        const { backlinks, author, ...postForExport } = post; // omit backlinks and author
+        const {backlinks, author, ...postForExport} = post; // omit backlinks and author
         if (postForExport.can) {
-          delete postForExport.can;
+            delete postForExport.can;
         }
 
         if (postForExport.user) {
-        //  delete postForExport.user;
+            //  delete postForExport.user;
         }
 
 
@@ -56,90 +63,131 @@ const exporter = posts => {
         return postForExport;
     });
 
-    jsonExport(postsForExport, { headers: ['id', 'title', 'description']  }, (err, csv) => {
+    jsonExport(postsForExport, {headers: ['id', 'title', 'description']}, (err, csv) => {
         downloadCSV(csv, 'ideas'); // download as 'posts.csv` file
     });
 };
 
 const Empty = (props) => {
-    const { basePath, resource } = useListContext();
+    const {basePath, resource} = useListContext();
     const classes = useStyles(props);
 
     return (
         <div>
-        <Box textAlign="center" m={1}>
-            <div className={classes.message}>
-              <Inbox className={classes.icon} />
+            <Box textAlign="center" m={1}>
+                <div className={classes.message}>
+                    <Inbox className={classes.icon}/>
 
-              <Typography variant="h4" paragraph>
-                  No ideas yet
-              </Typography>
-              <Typography variant="body1">
-                  Create one or import from a file
-              </Typography>
-            </div>
-            <div className={classes.toolbar}>
-              <CreateButton basePath={basePath} />
-              <ImportButton resource={resource} />
-            </div>
-        </Box>
+                    <Typography variant="h4" paragraph>
+                        No ideas yet
+                    </Typography>
+                    <Typography variant="body1">
+                        Create one or import from a file
+                    </Typography>
+                </div>
+                <div className={classes.toolbar}>
+                    <CreateButton basePath={basePath}/>
+                    <ImportButton resource={resource}/>
+                </div>
+            </Box>
         </div>
     );
 };
 
 
 export const ListActions = props => {
-  const {
-    className,
-    filters,
-    permanentFilter,
-    currentSort,
-    resource,
-    displayedFilters,
-    filterValues,
-    basePath,
-    showFilter,
-    total,
-  } = props;
+    const {
+        className,
+        filters,
+        permanentFilter,
+        currentSort,
+        resource,
+        displayedFilters,
+        filterValues,
+        basePath,
+        showFilter,
+        total,
+    } = props;
 
-  return (
-    <TopToolbar className={className}>
-    {filters && cloneElement(filters, {
+    return (
+        <TopToolbar className={className}>
+            {filters && cloneElement(filters, {
                 resource,
                 showFilter,
                 displayedFilters,
                 filterValues,
                 context: 'button',
             })}
-      <CreateButton basePath={basePath}  />
-      <ExportButton
-         exporter={exporter}
-         disabled={total === 0}
-         resource={resource}
-         sort={currentSort}
-         filter={{ ...filterValues, ...permanentFilter }}
-       />
-      <ImportButton resource={resource} />
-    </TopToolbar>
-  );
+            <CreateButton basePath={basePath}/>
+            <ExportButton
+                exporter={exporter}
+                disabled={total === 0}
+                resource={resource}
+                sort={currentSort}
+                filter={{...filterValues, ...permanentFilter}}
+            />
+            <ImportButton resource={resource}/>
+        </TopToolbar>
+    );
 };
 
-const PostBulkActionButtons = props => (
-  <Fragment>
-    <BulkDeleteButton  undoable={false} {...props} />
-    <RAButton onClick={props.handleBulkEditClick} label={'edit'} {...props}><ContentCreate /></RAButton>
-  </Fragment>
+const BulkEditButton = ({selectedIds}) => {
+    const [open, setOpen] = useState(false);
+    const refresh = useRefresh();
+    const notify = useNotify();
+    const unselectAll = useUnselectAll();
+    const [updateMany, { loading }] = useUpdateMany(
+        'ideas',
+        selectedIds,
+        { views: 0 },
+        {
+            onSuccess: () => {
+                refresh();
+                notify('Posts updated');
+                unselectAll('posts');
+            },
+            onFailure: error => notify('Error: posts not updated', 'warning'),
+        }
+    );
+    const handleClick = () => setOpen(true);
+    const handleDialogClose = () => setOpen(false);
+
+    const handleConfirm = () => {
+        updateMany();
+        setOpen(false);
+    };
+
+    return (
+        <Fragment>
+            <Button label="Bulk edit" onClick={handleClick} ><ContentCreate/></Button>
+            <Confirm
+                isOpen={open}
+                loading={loading}
+                title="Update View Count"
+                content="Are you sure you want to reset the views for these items?"
+                onConfirm={handleConfirm}
+                onClose={handleDialogClose}
+            />
+        </Fragment>
+    );
+}
+
+const ListBulkActionButtons = props => (
+    <Fragment>
+        <BulkDeleteButton undoable={false} {...props} />
+        <BulkEditButton {...props} />
+    </Fragment>
 );
 
 const IdeaFilters = (props) => (
-  <Filter {...props}>
-    <TextInput label="Id" source="id"  defaultValue="" />
-    <TextInput label="Status" source="status"  defaultValue="" />
-  </Filter>
+    <Filter {...props}>
+        <TextInput label="Id" source="id" defaultValue=""/>
+        <TextInput label="Status" source="status" defaultValue=""/>
+    </Filter>
 );
 
 const BulkEditDialog = (props) => {
-    const { onClose, selectedValue, open } = props;
+    const {onClose, selectedValue, open} = props;
 
     const handleClose = () => {
         onClose(selectedValue);
@@ -155,10 +203,10 @@ const BulkEditDialog = (props) => {
             <DialogTitle id="simple-dialog-title">Bulk edit</DialogTitle>
             <SimpleForm save={handleSubmit}>
                 <SelectInput source="status" choices={[
-                    { id: 'closed', name: 'Closed' },
-                    { id: 'open', name: 'Open' },
-                    { id: 'denied', name: 'Denied' },
-                ]} />
+                    {id: 'closed', name: 'Closed'},
+                    {id: 'open', name: 'Open'},
+                    {id: 'denied', name: 'Denied'},
+                ]}/>
                 {/*<ReferenceInput label="Status" source="ideaId" reference="idea" variant="outlined">*/}
                 {/*    <SelectInput optionText="status"/>*/}
                 {/*</ReferenceInput>*/}
@@ -169,31 +217,33 @@ const BulkEditDialog = (props) => {
 }
 
 export const IdeaList = (props) => {
-    const [open, setOpen] = React.useState(false);
+    // const [open, setOpen] = React.useState(false);
 
-    const handleBulkEditClick = () => {
-        setOpen(true);
-    };
-
-    const handleClose = (value) => {
-        setOpen(false);
-    };
+    // const handleBulkEditClick = () => {
+    //     setOpen(true);
+    // };
+    //
+    // const handleClose = (value) => {
+    //     setOpen(false);
+    // };
 
     return (
-    <Fragment>
-        <List {...props} filters={<IdeaFilters/>} actions={<ListActions/>}  bulkActionButtons={<PostBulkActionButtons handleBulkEditClick={handleBulkEditClick} />}
-              exporter={exporter} empty={<Empty />}>
-            <Datagrid>
-                <TextField source="id"/>
-                <ImageField source="extraData.images[0]" label="Image"/>
-                <TextField source="title"/>
-                <TextField source="status"/>
-                <TextField source="yes" />
-                <TextField source="no" />
-                <DateField source="createdAt"/>
-                <EditButton basePath="/idea"/>
-            </Datagrid>
-        </List>
-        <BulkEditDialog open={open} onClose={handleClose} />
-    </Fragment>
-)};
+        <Fragment>
+            <List {...props} filters={<IdeaFilters/>} actions={<ListActions/>}
+                  bulkActionButtons={<ListBulkActionButtons />}
+                  exporter={exporter} empty={<Empty/>}>
+                <Datagrid>
+                    <TextField source="id"/>
+                    <ImageField source="extraData.images[0]" label="Image"/>
+                    <TextField source="title"/>
+                    <TextField source="status"/>
+                    <TextField source="yes"/>
+                    <TextField source="no"/>
+                    <DateField source="createdAt"/>
+                    <EditButton basePath="/idea"/>
+                </Datagrid>
+            </List>
+            {/*<BulkEditDialog open={open} onClose={handleClose} />*/}
+        </Fragment>
+    )
+};
